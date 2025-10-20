3fcd030548069eaa6a3bf339b1aff73949332d4f
aryabadwe007 <aryabadwe2021@gmail.com>
2025-10-20 13:40:40 +0530

    first commit

--- Day 2/ArmstrongNumber.java
+++ Day 2/ArmstrongNumber.java
@@ -5,24 +5,20 @@ public class ArmstrongNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input
         System.out.print("Enter an integer: ");
         int number = scanner.nextInt();
 
         int original = number;
         int sum = 0;
 
-        // Count the number of digits
         int digits = String.valueOf(number).length();
 
-        // Calculate sum of each digit raised to the power of number of digits
         while (number != 0) {
             int digit = number % 10;
             sum += Math.pow(digit, digits);
             number /= 10;
         }
 
-        // Output
         if (sum == original) {
             System.out.println("It is an Armstrong number.");
         } else {
@@ -32,3 +28,4 @@ public class ArmstrongNumber {
         scanner.close();
     }
 }
+
